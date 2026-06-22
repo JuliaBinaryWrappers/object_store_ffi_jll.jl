0be687ff6c8d4424fb6ef482460ce9498f51dac7
jlbuild <juliabuildbot@gmail.com>
2026-06-22 16:46:02 +0000

    object_store_ffi_jll build 0.12.4+0

--- src/object_store_ffi_jll.jl
+++ src/object_store_ffi_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("object_store_ffi")
-JLLWrappers.@generate_main_file("object_store_ffi", UUID("0e112785-0821-598c-8835-9f07837e8d7b"))
+JLLWrappers.@generate_main_file("object_store_ffi", Base.UUID("0e112785-0821-598c-8835-9f07837e8d7b"))
 end  # module object_store_ffi_jll
